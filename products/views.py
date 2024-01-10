ddc0571f9f205c2c1994f7df6a9c38a412ed4125
Slava B <slbabin@gmail.com>
2024-01-10 19:41:03 +0000

    Added products view and templates

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Poster
 
-# Create your views here.
+def all_posters(request):
+    """ A view to display all posters"""
+
+    posters= Poster.objects.all()
+
+    context = {
+        'posters' : posters, 
+    }
+    
+    return render(request, 'products/posters.html', context)
