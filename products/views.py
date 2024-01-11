4616e206dac860f85020db1ce3f68ec95d972572
Slava B <slbabin@gmail.com>
2024-01-11 21:35:16 +0000

    Added search function

--- products/views.py
+++ products/views.py
@@ -1,13 +1,29 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Poster
 
 def all_posters(request):
     """ A view to display all posters"""
 
     posters= Poster.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error("request, You didn't enter any search query")
+                return redirect(reverse('posters'))
+
+            queries = Q(title__icontains=query) | Q(description__icontains=query)
+            posters = posters.filter(queries)
+
+
 
     context = {
         'posters' : posters, 
+        'search_term': query,
     }
     
     return render(request, 'products/posters.html', context)
