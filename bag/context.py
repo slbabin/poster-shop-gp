bd717b4326252417a7cd2ef4d4f43f1dcb7f2301
Slava B <slbabin@gmail.com>
2024-01-17 18:27:37 +0000

    Added a function to context.py to display bag items

--- bag/context.py
+++ bag/context.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Poster
 
 
 def bag_content(request):
@@ -7,6 +9,18 @@ def bag_content(request):
     bag_items = []
     total = 0
     product_count = 0
+
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Poster, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product':product,
+        })
 
     free_delivery_offer = 0
     FREE_DELIVERY_THRESHOLD = 0
