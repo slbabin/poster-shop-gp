705d75d64f70d40275dc18ee632b7ecf8c12ae0c
Slava B <slbabin@gmail.com>
2024-01-17 20:34:01 +0000

    Added sizes field to the Poster model

--- products/models.py
+++ products/models.py
@@ -23,6 +23,7 @@ class Poster(models.Model):
     artist = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     year = models.PositiveIntegerField()
+    sizes = models.BooleanField(default=False, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
