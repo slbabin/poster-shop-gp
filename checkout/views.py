830b8f0ce2555a6825dbcdb09a8523e2c84038f5
Slava B <slbabin@gmail.com>
2024-01-25 21:24:56 +0000

    Added checkout funtions

--- checkout/views.py
+++ checkout/views.py
@@ -4,10 +4,12 @@ from django.conf import settings
 
 from .forms import OrderForm
 from bag.context import bag_contents
-
-import Stripe
+import stripe
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "You bag is empty!")
@@ -16,13 +18,24 @@ def checkout(request):
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+
+    )
+
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'No Stripe public key.')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51OIzlALQubLimFj0XbqJIUUD8Am6V2rXpRgu9It8E0t2xB1KHWpbtnsZop8uS6hSA0piAB819tAE3DAsDMFWLuUE008j15E3si',
-        'client_secret': 'test client secret ',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)    
