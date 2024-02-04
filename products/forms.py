97c019c9d052d74a47e05f3b44eb3f93983391ce
Slava B <slbabin@gmail.com>
2024-02-04 17:06:45 +0000

    Added a widget to the image field

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Poster, Category
+from .widgets import CustomClearableFileInput
 
 
 class PosterForm(forms.ModelForm):
@@ -8,6 +9,8 @@ class PosterForm(forms.ModelForm):
     class Meta:
         model = Poster
         fields = '__all__'
+
+    image =  forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
