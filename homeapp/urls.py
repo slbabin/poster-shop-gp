0c22faf5e56fa147062dc8ac78f8bc78e9573a9a
Slava B <slbabin@gmail.com>
2024-02-08 23:03:54 +0000

    Added additional pages, about, contact, return, terms

--- homeapp/urls.py
+++ homeapp/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home' ),
-    
+    path('terms/', views.terms, name='terms' ),
+    path('about/', views.about, name='about' ),
+    path('return/', views.returnpolicy, name='return' ),
+    path('contact/', views.contact, name='contact' ),
     
 ]
--- homeapp/views.py
+++ homeapp/views.py
@@ -4,3 +4,27 @@ def index(request):
     """ A view to return the index page """
     
     return render(request, 'homeapp/index.html')
+
+
+def terms(request):
+    """ A view to return terms page """
+    
+    return render(request, 'homeapp/terms.html')
+
+
+def about(request):
+    """ A view to return about page  """
+    
+    return render(request, 'homeapp/about-company.html')
+
+
+def returnpolicy (request):
+    """ A view to return return page  """
+    
+    return render(request, 'homeapp/return.html')
+
+
+def contact (request):
+    """ A view to return contact page  """
+    
+    return render(request, 'homeapp/contact.html')    
