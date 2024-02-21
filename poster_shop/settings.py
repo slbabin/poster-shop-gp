c0baab9e0eb6df1fbb1949578af71a920665f479
Slava B <slbabin@gmail.com>
2024-02-21 22:52:20 +0000

    Set debug to false

--- poster_shop/settings.py
+++ poster_shop/settings.py
@@ -22,13 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'sdfsfa')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
 # DEBUG = 'DEVELOPMENT' in os.environ
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['poster-shop-4968517d882f.herokuapp.com',
                  '8000-slbabin-postershopgp-67nll0kepo4.ws-eu108.gitpod.io']
